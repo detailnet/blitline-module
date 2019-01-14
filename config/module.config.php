dd3c8d56df3afaf14c04b33bab57e76370ad2c01
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2019-01-14 09:49:25 +0100

    Fixed namespace for blitline client configuration

--- config/module.config.php
+++ config/module.config.php
@@ -11,7 +11,7 @@ return [
             Blitline\Job\JobBuilder::CLASS => Blitline\Job\JobBuilder::CLASS,
         ],
         'factories' => [
-            Blitline\Client\BlitlineClient::CLASS => Factory\Client\BlitlineClientFactory::CLASS,
+            Blitline\BlitlineClient::CLASS => Factory\BlitlineClientFactory::CLASS,
             Blitline\Options\ModuleOptions::CLASS => Factory\Options\ModuleOptionsFactory::CLASS,
         ],
         'initializers' => [],
